95085e7f509918e8c359b40b9fb2d68a3a5bb005
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2018-11-07 23:02:20 +0700

    update new front end app#1

--- front end/#app1/index.app.js
+++ front end/#app1/index.app.js
@@ -1,16 +1,36 @@
-$(function() {
-    // alert('msg');
 
+$(".btn-submit").click(function(){
+    var request = createJSON();
+    var info_url = "http://localhost:8080/Request/add";
     $.ajax({
-        url: 'http://localhost:3000/api/products',
-        type: 'GET',
-        dataType: 'json',
-        timeout: 10000
-    }).done(function(data) {
-        // console.log(data);
-        var source = document.getElementById("product-template").innerHTML;
-        var template = Handlebars.compile(source);
-        var html = template(data);
-        $('#products').html(html);
-    })
+
+        type: "POST",
+        url : info_url,
+        data: JSON.stringify(request),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        processData: false,
+        success: function( data, textStatus, jQxhr ){
+            $('#response pre').html( JSON.stringify( data ) );
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
 });
+
+function createJSON() {
+    if($("#Name").val()=="" || $("#Phone").val()=="" || $("#Address").val()=="" || $("#Notes").val()=="")
+        return;
+    var d = new Date();
+    var obj = new Object();
+    obj.name = $("#Name").val();
+    obj.phone  = $("#Phone").val();
+    obj.address = $("#Address").val();
+    obj.note =  $("#Notes").val();
+    obj.position = null;
+    obj.status =null;
+    obj.time = d.getTime();
+    obj.id_drive =null;
+    return obj;
+}
